fca99efac3fc34445a4408e420ea7a91ac5b6e7d
GunnCode <gunncode@gmail.com>
2022-01-03 17:22:22 -0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Post content and earn Bitcoin</p>
+<form>
+	<input type="email"/>
+	<button type="submit">Subscribe</button>
+</form>
+	
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #0b82de;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
